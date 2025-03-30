411114282dbcf72280a99387b53ae5f359ced436
LPKInvestmentGroup <150193373+LPKInvestmentGroup@users.noreply.github.com>
2025-03-30 18:31:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function validateForm() {
     const blockedEmails = ['blocked@example.com', 'spam@example.com', 'ericjonesmyemail@gmail.com'];
 
     // Keywords to reject in the message
-    const blockedKeywords = ['spam', 'prohibited', 'LeadConnect', '14-day trial', 'Web Visitors Into Leads'];
+    const blockedKeywords = ['14-day trial', 'Web Visitors Into Leads'];
     //const blockedKeywords = ['14-day trial', 'Web Visitors Into Leads'];
 
     // Check if email is in the blocked list
